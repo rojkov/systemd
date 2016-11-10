6bd7049a4f645a79f675c0d867ac53ca059e3f76
Khem Raj <raj.khem@gmail.com>
2016-11-09 20:49:53 -0800

    socket-util: don't fail if libc doesn't support IDN
    Upstream-Status: Pending
    
    Signed-off-by: Emil Renner Berthing <systemd@esmil.dk>
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    
    %% original patch: 0019-socket-util-don-t-fail-if-libc-doesn-t-support-IDN.patch

--- src/basic/socket-util.c
+++ src/basic/socket-util.c
@@ -47,6 +47,15 @@
 #include "user-util.h"
 #include "utf8.h"
 #include "util.h"
+/* Don't fail if the standard library
+ * doesn't support IDN */
+#ifndef NI_IDN
+#define NI_IDN 0
+#endif
+
+#ifndef NI_IDN_USE_STD3_ASCII_RULES
+#define NI_IDN_USE_STD3_ASCII_RULES 0
+#endif
 
 int socket_address_parse(SocketAddress *a, const char *s) {
         char *e, *n;
